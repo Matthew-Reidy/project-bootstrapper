769f6334c26400fd19d503606b741be6a2805517
Matthew Reidy <matthewreidy5@gmail.com>
2023-07-31 19:31:07 -0700

    fixed subprocess run commands, added git remote -v command and printed output, re added try except block

--- script.py
+++ script.py
@@ -4,25 +4,32 @@ import os
 import sys 
 
 def run() -> None:
-    if len(sys.argv) > 3:
-        print("Only 3 arguments allowed!")
+    try:
+        if len(sys.argv) > 3:
+            print("Only 3 arguments allowed!")
 
- 
+        specifiedDir: str = sys.argv[1]
+        os.mkdir(specifiedDir)
+        os.chdir(specifiedDir)
+        subprocess.run(args=["git init -b main"], shell=True)
+        subprocess.run(args=f"git remote add origin {sys.argv[2]}", shell=True)
+        info = subprocess.run(args=["git remote -v"], shell=True, capture_output=True, text=True)
+        print(info)
 
-    specifiedDir: str = sys.argv[1]
-    os.mkdir(specifiedDir)
-    os.chdir(specifiedDir)
-    subprocess.run(args="git init -b main")
-    subprocess.run(args=f"git remote add origin {sys.argv[2]}")
-    with open(".gitignore", "w") as gitignore:
-        gitignore.write("# ignored files go in here \n __pycache__")
-    with open("README.MD", "w" ) as readme:
-        readme.write("# Hello, World!")
-    subprocess.run(args="git add .")
-    subprocess.run(args='git commit -m "First Commit"')
-    subprocess.run(args="git push origin main")
+        with open(".gitignore", "w") as gitignore:
+            gitignore.write("# ignored files go in here \n __pycache__")
+
+        with open("README.MD", "w" ) as readme:
+            readme.write("# Hello, World!")
+
+        subprocess.run(args=["git add ."], shell=True)
+        subprocess.run(args=['git commit -m "First Commit"'], shell=True)
+        subprocess.run(args=["git push origin main"], shell=True)
+    except IndexError:
+        print("missing arguments!")
 
     
 
 
 run()
+
